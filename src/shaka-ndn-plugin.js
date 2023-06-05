4ce20aef8ca5d363b13f6aa7b1fb51730f539685
Junxiao Shi <git@mail1.yoursunny.com>
2023-06-05 20:32:35 +0000

    shaka-ndn-plugin: delete discoverVersion
    The plugin now expects the server to support ndn6-file-server protocol.
    CanBePrefix version discovery and final segment estimation are
    eliminated.

--- src/shaka-ndn-plugin.js
+++ src/shaka-ndn-plugin.js
@@ -1,11 +1,10 @@
 import { Endpoint } from "@ndn/endpoint";
+import { FileMetadata } from "@ndn/fileserver";
 import { FwHint, Name } from "@ndn/packet";
 import { retrieveMetadata } from "@ndn/rdr";
-import { discoverVersion, fetch, RttEstimator, TcpCubic } from "@ndn/segmented-object";
-import { assert } from "@ndn/util";
+import { fetch, RttEstimator, TcpCubic } from "@ndn/segmented-object";
 import hirestime from "hirestime";
 import * as log from "loglevel";
-import DefaultMap from "mnemonist/default-map.js";
 import PQueue from "p-queue";
 import shaka from "shaka-player";
 
@@ -44,14 +43,9 @@ const getNow = hirestime();
 
 class VideoFetcher {
   constructor() {
-    /** @type {boolean | undefined} */
-    this.perFileMetadata = undefined;
-    /** @type {import("@ndn/packet").Component | undefined} */
-    this.versionComponent = undefined;
     this.queue = new PQueue({ concurrency: 4 });
     this.rtte = new RttEstimator({ maxRto: 10000 });
     this.ca = new TcpCubic({ c: 0.1 });
-    this.estimatedCounts = new DefaultMap(() => 5);
   }
 }
 
@@ -66,7 +60,6 @@ class FileFetcher {
     this.uri = uri;
     this.requestType = requestType;
     this.name = new Name(uri.replace(/^ndn:/, ""));
-    this.estimatedCountKey = this.name.getPrefix(-2).valueHex;
 
     this.abort = new AbortController();
     this.endpoint = new Endpoint({
@@ -74,70 +67,24 @@ class FileFetcher {
       retx: 10,
       signal: this.abort.signal,
     });
-
-    /** @type {Name | undefined} */
-    this.versioned = undefined;
   }
 
-  get estimatedFinalSegNum() {
-    return this.vf.estimatedCounts.get(this.estimatedCountKey);
-  }
+  async retrieve() {
+    const metadata = await retrieveMetadata(this.name, FileMetadata, {
+      endpoint: this.endpoint,
+    });
 
-  set estimatedFinalSegNum(value) {
-    this.vf.estimatedCounts.set(this.estimatedCountKey, value);
-  }
-
-  async discoverConvention() {
-    const result = await Promise.race([
-      discoverVersion(this.name, {
-        endpoint: this.endpoint,
-      }),
-      retrieveMetadata(this.name, {
-        endpoint: this.endpoint,
-      }),
-    ]);
-    if (result instanceof Name) {
-      this.vf.perFileMetadata = false;
-      this.vf.versionComponent = result.get(-1);
-      this.versioned = result;
-      log.debug(`NdnPlugin(${this.name}) convention version=${this.vf.versionComponent.toString()}`);
-    } else {
-      this.vf.perFileMetadata = true;
-      this.vf.versionComponent = undefined;
-      this.versioned = result.name;
-      log.debug(`NdnPlugin(${this.name}) convention perFileMetadata`);
-    }
-  }
-
-  async discoverVersion() {
-    if (this.versioned) {
-      return;
-    }
-
-    if (this.vf.perFileMetadata) {
-      this.versioned = (await retrieveMetadata(this.name, {
-        endpoint: this.endpoint,
-      })).name;
-    } else {
-      assert(this.vf.versionComponent);
-      this.versioned = this.name.append(this.vf.versionComponent);
-    }
-  }
-
-  async download() {
     const t0 = getNow();
-    const result = fetch(this.versioned, {
+    const payload = await fetch(metadata.name, {
       endpoint: this.endpoint,
       rtte: this.vf.rtte,
       ca: this.vf.ca,
       retxLimit: 4,
-      estimatedFinalSegNum: this.estimatedFinalSegNum,
+      estimatedFinalSegNum: metadata.lastSeg,
     });
-    const payload = await result;
 
     const timeMs = getNow() - t0;
-    this.estimatedCounts = result.count;
-    log.debug(`NdnPlugin(${this.name}) download rtt=${Math.round(timeMs)} count=${result.count}`);
+    log.debug(`NdnPlugin(${this.name}) retrieve rtt=${Math.round(timeMs)} len=${payload.length}`);
     sendBeacon({
       a: "F",
       n: `${this.name}`,
@@ -188,11 +135,7 @@ export function NdnPlugin(uri, request, requestType) {
   return new shaka.util.AbortableOperation(vf.queue.add(async () => {
     log.debug(`NdnPlugin(${ff.name}) dequeue waited=${getNow() - t0}`);
     try {
-      if (vf.perFileMetadata === undefined) {
-        await ff.discoverConvention();
-      }
-      await ff.discoverVersion();
-      return await ff.download();
+      return await ff.retrieve();
     } catch (err) {
       ff.handleError(err);
     }
